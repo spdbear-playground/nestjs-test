74dc0d607c3b662184f00401ede1afb2c0064013
spdbear <16793732+spdbear@users.noreply.github.com>
2021-04-13 00:44:11 +0900

    モックデータを返す

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
-
+import { CARS } from './cars.mock';
 @Injectable()
 export class CarService {
+  private cars = CARS;
   public async getCars() {
-    return {};
+    return this.cars;
   }
 }
---
+++ src/car/cars.mock.ts
@@ -0,0 +1,8 @@
+export const CARS = [
+  {
+    id: '1555555555',
+    brand: 'Toyota',
+    color: 'white',
+    model: 'Supra',
+  },
+];

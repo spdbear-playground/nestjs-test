7009a680c69b3adca6808a1a187d15550c7b32a1
spdbear <16793732+spdbear@users.noreply.github.com>
2021-04-13 01:19:18 +0900

    雑にビジネスロジックを実装

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -1,9 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CARS } from './cars.mock';
 @Injectable()
 export class CarService {
   private cars = CARS;
-  public async getCars() {
+  public getCars() {
     return this.cars;
+  }
+
+  public postCar(car: {
+    id: number;
+    brand: string;
+    color: string;
+    model: string;
+  }) {
+    return this.cars.push(car);
+  }
+  public getCarById(id: number) {
+    const car = this.cars.find((car) => car.id === id);
+    if (!car) {
+      throw new HttpException('NOT FOUND', 404);
+    }
+    return car;
+  }
+  public deleteCarById(id: number) {
+    const carIndex = this.cars.findIndex((car) => car.id === id);
+    if (carIndex === -1) {
+      throw new HttpException('NOT FOUND', 404);
+    }
+    this.cars.splice(carIndex, 1);
+    return this.cars;
+  }
+  public putCarById(id: number, propertyName: string, propertyValue: string) {
+    const carIndex = this.cars.findIndex((car) => car.id === id);
+    if (carIndex === -1) {
+      throw new HttpException('NOT FOUND', 404);
+    }
+    this.cars[carIndex][propertyName] = propertyValue;
+    return;
   }
 }
